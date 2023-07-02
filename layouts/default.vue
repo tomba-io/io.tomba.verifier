b12dd4d9b9f90f6f06bd51a64c6a2dff667d9e11
Mohamed Ben <b.mohamed@tomba.io>
2023-07-02 12:27:39 +0100

    feat: update layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,18 @@
       app
     >
       <v-list>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="/logo.svg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Tomba</v-list-item-title>
+            <v-list-item-subtitle>Email Verifier</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider />
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -26,7 +38,7 @@
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
       <v-btn icon @click.stop="clipped = !clipped">
@@ -34,33 +46,48 @@
       </v-btn>
       <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    <!-- <v-footer :absolute="!fixed" app>
+      <v-bottom-navigation
+        absolute
+        hide-on-scroll
+        horizontal
+        scroll-target="#hide-on-scroll-example"
+      >
+        <v-btn text>
+          <span>Recents</span>
+
+          <v-icon>mdi-history</v-icon>
+        </v-btn>
+
+        <v-btn text>
+          <span>Favorites</span>
+
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn text>
+          <span>Nearby</span>
+
+          <v-icon>mdi-map-marker</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+
+      <v-responsive
+        id="hide-on-scroll-example"
+        class="overflow-y-auto"
+        max-height="600"
+      >
+        <v-responsive></v-responsive>
+      </v-responsive>
+       <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer> -->
   </v-app>
 </template>
 
@@ -73,21 +100,40 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home',
+          title: 'Home',
           to: '/',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          icon: 'mdi-account',
+          title: 'Account',
+          to: '/account',
+        },
+        {
+          icon: 'mdi-connection',
+          title: 'Connection',
+          to: '/connection',
+        },
+        {
+          icon: 'mdi-history',
+          title: 'History',
+          to: '/history',
+        },
+        {
+          icon: 'mdi-information',
+          title: 'About',
+          to: '/about',
         },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'Tomba',
     }
   },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
 }
 </script>
