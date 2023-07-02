2851cef4ab2b5a88afee74110515813254d50602
Mohamed Ben <b.mohamed@tomba.io>
2023-07-02 12:28:10 +0100

    feat: nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'spa',
+  ssr: false,
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
@@ -16,28 +16,38 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    titleTemplate: '%s - ' + 'Tomba ',
+    title: 'Email Verifier',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }],
   },
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['@mdi/font/css/materialdesignicons.css'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [],
+  plugins: [{
+    src: '@plugins/axios',
+  }, ],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
@@ -48,7 +58,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify',
   ],
   /*
@@ -68,19 +78,36 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
+    defaultAssets: false,
+    icons: {
+      iconfont: 'mdi',
+    },
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+        light: {
+          primary: '#1853db',
+          secondary: colors.lightBlue.base,
+          accent: colors.orange.base,
+          error: colors.red.base,
+          warning: colors.yellow.base,
+          info: colors.indigo.base,
+          success: colors.green.base,
+          toolbar: '#1853db',
         },
+        dark: {
+          primary: '#1853db',
+          secondary: colors.lightBlue.base,
+          accent: colors.orange.base,
+          error: colors.red.base,
+          warning: colors.yellow.base,
+          info: colors.indigo.base,
+          success: colors.green.base,
+          toolbar: colors.grey.darken4,
+        },
+      },
+      options: {
+        customProperties: true,
       },
     },
   },
@@ -93,7 +120,10 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, { isDev, isClient }) {
+    extend(config, {
+      isDev,
+      isClient
+    }) {
       if (!isDev) {
         // relative links, please.
         config.output.publicPath = './assets/'
